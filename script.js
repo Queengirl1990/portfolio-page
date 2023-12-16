0b396db82dda882d0906e37569ba7917edd83679
Queengirl1990 <bruehlanja90@gmail.com>
2023-12-16 15:58:35 +0100

    ende des tuts

--- script.js
+++ script.js
@@ -1,6 +1,14 @@
 let sections = document.querySelectorAll('section');
 let navLinks = document.querySelectorAll('header nav a');
 
+let menuIcon = document.querySelector('#menu-icon');
+let navbar = document.querySelector('.navbar');
+
+menuIcon.onclick = () => {
+    menuIcon.classList.toggle('bx-x');
+    navbar.classList.toggle('active');
+}
+
 window.onscroll = () => {
     sections.forEach(sec => {
         let top = window.scrollY;
