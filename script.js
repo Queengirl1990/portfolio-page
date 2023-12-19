cecf57426cc096b18cbcd2ffc2034dff3b7a07a7
Queengirl1990 <bruehlanja90@gmail.com>
2023-12-19 22:22:13 +0100

    andi

--- script.js
+++ script.js
@@ -1,26 +1,65 @@
-let sections = document.querySelectorAll('section');
-let navLinks = document.querySelectorAll('header nav a');
+document.addEventListener('DOMContentLoaded', () => {
+    const portfolioContainer = document.querySelector('.portfolio-container');
+    const arrowLeft = document.querySelector('.fa-arrow-left');
+    const arrowRight = document.querySelector('.fa-arrow-right');
 
-let menuIcon = document.querySelector('#menu-icon');
-let navbar = document.querySelector('.navbar');
+    arrowLeft.addEventListener('click', () => {
+        // Logik für die Verschiebung zur vorherigen Card
+        portfolioContainer.scrollBy({
+            left: -300, // Ändere dies entsprechend der Breite deiner Cards
+            behavior: 'smooth',
+        });
+    });
 
-menuIcon.onclick = () => {
-    menuIcon.classList.toggle('bx-x');
-    navbar.classList.toggle('active');
-}
+    arrowRight.addEventListener('click', () => {
+        // Logik für die Verschiebung zur nächsten Card
+        portfolioContainer.scrollBy({
+            left: 300, // Ändere dies entsprechend der Breite deiner Cards
+            behavior: 'smooth',
+        });
+    });
 
-window.onscroll = () => {
-    sections.forEach(sec => {
-        let top = window.scrollY;
-        let offset = sec.offsetTop - 150;
-        let height = sec.offsetHeight;
-        let id = sec.getAttribute('id');
-
-        if (top >= offset && top < offset + height) {
-            navLinks.forEach(link => {
-                link.classList.remove('active');
+    // Füge eine Event-Listener-Funktion hinzu, um die Pfeiltasten der Tastatur zu verwenden
+    document.addEventListener('keydown', (event) => {
+        if (event.key === 'ArrowLeft') {
+            // Logik für die Verschiebung zur vorherigen Card
+            portfolioContainer.scrollBy({
+                left: -300, // Ändere dies entsprechend der Breite deiner Cards
+                behavior: 'smooth',
             });
-            document.querySelector('header nav a[href*=' + id + ']').classList.add('active');
+        } else if (event.key === 'ArrowRight') {
+            // Logik für die Verschiebung zur nächsten Card
+            portfolioContainer.scrollBy({
+                left: 300, // Ändere dies entsprechend der Breite deiner Cards
+                behavior: 'smooth',
+            });
         }
     });
-};
+
+    let sections = document.querySelectorAll('section');
+    let navLinks = document.querySelectorAll('header nav a');
+
+    let menuIcon = document.querySelector('#menu-icon');
+    let navbar = document.querySelector('.navbar');
+
+    menuIcon.onclick = () => {
+        menuIcon.classList.toggle('bx-x');
+        navbar.classList.toggle('active');
+    };
+
+    window.onscroll = () => {
+        sections.forEach(sec => {
+            let top = window.scrollY;
+            let offset = sec.offsetTop - 150;
+            let height = sec.offsetHeight;
+            let id = sec.getAttribute('id');
+
+            if (top >= offset && top < offset + height) {
+                navLinks.forEach(link => {
+                    link.classList.remove('active');
+                });
+                document.querySelector('header nav a[href*=' + id + ']').classList.add('active');
+            }
+        });
+    };
+});
